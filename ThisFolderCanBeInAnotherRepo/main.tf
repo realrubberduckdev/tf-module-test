b29dbe8b33ee78d42c28087249fa50e340e9256a
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2021-04-03 19:27:56 +0100

    Use git commit reference

--- ThisFolderCanBeInAnotherRepo/main.tf
+++ ThisFolderCanBeInAnotherRepo/main.tf
@@ -2,14 +2,20 @@ provider "azurerm" {
     features {}
 }
 
+locals {
+  tags = {
+    "key1" = "value1"
+  }
+}
+
 module "resource_group" {
-  source = "git::https://github.com/realrubberduckdev/tf-module-test.git//modules//ResourceGroup"
+  source = "git::https://github.com/realrubberduckdev/tf-module-test.git//modules//ResourceGroup?ref=9451c9a386b411b71400aa1a382c3e93b2b9e9a0"
   name     = "tfmoduletest3"
   location = "North Europe"
 }
 
 module "storage_account" {
-  source = "git::https://github.com/realrubberduckdev/tf-module-test.git//modules//StorageAccount"
+  source = "git::https://github.com/realrubberduckdev/tf-module-test.git//modules//StorageAccount?ref=9451c9a386b411b71400aa1a382c3e93b2b9e9a0"
   name     = "dpteststrgacc1"
   resource_group_name = module.resource_group.name
   location = "North Europe"
